3016b6d6512130502bfb8b36b255759a4ff00d5f
andridm_ <64353589+andridm14@users.noreply.github.com>
2023-09-23 14:22:26 +0700

    update activity, add file, icon

--- app/src/main/java/com/andridm/kampusapp/AboutActivity.kt
+++ app/src/main/java/com/andridm/kampusapp/AboutActivity.kt
@@ -1,11 +1,27 @@
 package com.andridm.kampusapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menubar_about, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.home -> startActivity(Intent(this@AboutActivity, MainActivity::class.java))
+
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
